7865e87643ef00a16c31887f1805c8d4083b6e36
amq001 <aqibmehtab001@gmail.com>
2023-12-16 01:07:14 +0500

    lecture 5 added

---
+++ 5_ch01_ps/script.js
@@ -0,0 +1,24 @@
+//  Q1
+let a = "harry"
+let b = 5
+console.log(a+b)
+
+//  Q2
+console.log(typeof a+b)
+console.log(typeof(a+b))
+
+//  Q3
+const hello = {
+  a : 3,
+  b : 5,
+  c : 7
+}
+console.log(hello)
+
+//  Q4
+hello["d"] = 5
+console.log(hello)
+
+//  Q5
+console.log(hello.a)
+console.log(hello["a"])
