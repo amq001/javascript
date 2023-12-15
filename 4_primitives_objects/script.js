cd0fb020275eb9245b30e2f8a4a0dbc65905234d
amq001 <aqibmehtab001@gmail.com>
2023-12-16 01:06:17 +0500

    lecture 4 added

---
+++ 4_primitives_objects/script.js
@@ -0,0 +1,18 @@
+// Primitive datatype
+let a = null
+let b = 345
+let c = true
+let d = BigInt("567")
+let e = "harry"
+let f = Symbol("It is a symbol")
+let g = undefined
+console.log(a,b,c,d,e,f,g)
+
+// Non primitive datatype
+const stuff = {
+  "Aqib" : 01,
+  "Anas" : 02,
+  "Zia" : 03,
+  "Abdullah" : 04,
+}
+console.log(stuff)
