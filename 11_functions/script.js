741b2f8ccb8301d97a13594e36226a67271e4eef
amq001 <aqibmehtab001@gmail.com>
2023-12-18 11:52:36 +0500

    lecture 7 added

---
+++ 11_functions/script.js
@@ -0,0 +1,22 @@
+//  function
+function OnePlusAvg(x,y){
+    console.log("Done")
+    return 1 + (x+y)/2
+  }
+  let a = 1
+  let b = 2
+  let c = 3
+  console.log(OnePlusAvg(a,b))
+  console.log(OnePlusAvg(b,c))
+  console.log(OnePlusAvg(a,c))
+  
+  //  Arrow function
+  const sum = (p,q,r)=>{
+    return (p+q+r)
+  }
+  console.log(sum(a,b,c))
+  
+  const hello = ()=>{
+    console.log("jaa na bhai")
+  }
+  hello();
