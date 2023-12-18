4da5f1f35c60fdbec7e9407360c8c604e91409a2
amq001 <aqibmehtab001@gmail.com>
2023-12-19 00:34:46 +0500

    lecture 17 added

---
+++ 17_array_methods/script.js
@@ -0,0 +1,26 @@
+let num = [1,2,3,4]
+let b = num.toString()
+console.log(num)
+console.log(typeof num)
+console.log(typeof b)
+let x = num.join("_")
+console.log(x)
+console.log(typeof x)
+
+//  pop
+let z = num.pop()
+console.log(num)
+console.log(z)
+
+//  push
+let a = num.push(55,55)
+console.log(num)
+console.log(a)
+
+//  shift
+let g = num.shift()
+console.log(num,g)
+
+//  unshift
+let h = num.unshift(88)
+console.log(num,h)
