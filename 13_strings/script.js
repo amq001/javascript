0489b1f925d28392776c61c25c325e55885521f4
amq001 <aqibmehtab001@gmail.com>
2023-12-19 00:30:34 +0500

    lecture 13 added

---
+++ 13_strings/script.js
@@ -0,0 +1,14 @@
+let a = "Aqib"
+let b = 'mehtab'
+console.log(a[2])
+console.log(b[2])
+
+//  template literals
+let c = "aqib"
+let d = "anas"
+let sen = `${c} is a friend of ${d}`
+console.log(sen)
+
+//  escape sequence character
+
+// \n , \r , \' , \"
