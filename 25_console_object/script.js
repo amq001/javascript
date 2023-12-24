517735fdba849de3be91b1b170bb705728836a32
amq001 <aqibmehtab001@gmail.com>
2023-12-24 22:14:32 +0500

    lecture 25 added

---
+++ 25_console_object/script.js
@@ -0,0 +1,26 @@
+console.log(console)
+console.error("error hai bay")
+console.log("hello")
+console.assert(5>52)
+console.assert(55>52)
+console.assert
+//console.clear()
+let obj = {a:1,b:2,c:3}
+console.table(obj)
+console.warn("dont drink pepsi")
+
+
+console.log("log")
+console.info("info")
+console.warn("warn")
+console.error("err")
+console.assert("err"!=false)
+console.assert("err"==false)
+console.time("a")
+console.timeEnd("a")
+
+console.time("forloop")
+for(let i=0; i<5;i++){
+  console.log("1")
+}
+console.timeEnd("forloop")
