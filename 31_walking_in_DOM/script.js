2fc647f471ef5ae86ad6aecc664fb1cad7c9e106
amq001 <aqibmehtab001@gmail.com>
2023-12-26 01:40:17 +0500

    Lecture 31 added

---
+++ 31_walking_in_DOM/script.js
@@ -0,0 +1,13 @@
+document.head
+document.body
+document.title
+document.getElementsByClassName("cursor-pointer")
+document.getElementsByClassName("cursor-pointer")[6].style.color = "red"
+document.title = "hellooo"
+
+typeof document.title
+// string
+typeof document.documentElement
+// object page html taf
+
+console.log(document.body)
