64493dd1e3c967aaf8f108e09041c82471f3cb6b
amq001 <aqibmehtab001@gmail.com>
2023-12-28 16:20:29 +0500

    Lecture 36 added

---
+++ 36_searching_the_dom/script.js
@@ -0,0 +1,41 @@
+let ce = document.getElementById("xyz")
+console.log(ce)
+// ce.style.color = "yellow"
+
+// OTHER
+
+// let ctitles = document.querySelectorAll(".card-body")
+// ctitles[0].style.color = "blue"
+// ctitles[0].style.color = "red"
+// ctitles[0].style.color = "yellow"
+// console.log(ctitles)
+// let ct = document.getElementsByClassName("card-title")
+// ct[0].style.color = "green"
+// ct[1].style.color = "blue"
+// ct[2].style.color = "red"
+// ct.style.color = "red"
+
+// let id1 = document.getElementById("id_1")
+// id1.style.color = "red"
+
+// query selector is for css so use "." and "#"
+// let cts = document.querySelectorAll(".card-title")
+// cts[0].style.color = "blue"
+// cts[1].style.color = "red"
+// cts[2].style.color = "green"
+// console.log(cts)
+
+// document.querySelector(".this").style.color = "black"
+// document.querySelector(".this").style.background = "red"
+
+console.log(document.getElementsByTagName("a"))
+console.log(document.getElementsByName("search"))
+console.log(document.querySelector(".card").getElementsByTagName("a"))
+
+
+
+
+
+
+
+
