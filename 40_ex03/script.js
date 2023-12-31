e01a50e8ca3904c597574888622146ed85c7c631
amq001 <aqibmehtab001@gmail.com>
2024-01-01 01:37:19 +0500

    Lecture 40 added

---
+++ 40_ex03/script.js
@@ -0,0 +1,24 @@
+console.log("")
+$0.innerhtml
+$0.innerHTML = `Hey I am Aqib`
+
+[`1. What did the horse say after it tripped?
+Help! I’ve fallen and I can’t giddyup!`,
+`2. Why can’t you hear a pterodactyl going to the bathroom?
+Because the “P” is silent.`,
+`3. What do you call a well-balanced horse?
+Stable.`,
+`4. What do you call an angry carrot?
+A steamed veggie.`,
+`5. Where do polar bears keep their money?
+In a snowbank.`,
+`6. How do you make an egg-roll?
+You push it!`,
+`7. What would bears be without bees?
+Ears.`,
+`8. What do you call a pile of cats?
+A meow-ntain.`,
+`9. Why do cows wear bells?
+Because their horns don’t work.`,
+`10. Why did the bicycle fall over?
+Because it was two tired.`]
