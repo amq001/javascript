724e2e1265a9fe76d4caf6927ff552b3e8246b56
amq001 <aqibmehtab001@gmail.com>
2024-01-09 15:10:21 +0500

    lecture 41 added

---
+++ 41_inner_outer_html/script.js
@@ -0,0 +1,23 @@
+// let x = document.getElementsByTagName("span")[0]
+// let y = document.getElementsByTagName("span")[0]
+// console.dir(x)
+// console.log(y)
+
+// console.log(document.body.firstChild.nodeName)
+// console.log(document.body.firstElementChild.nodeName)
+
+// first.innerHTML
+// first.innerHTML = "<i>Hey I am Italic</i>"
+
+// first.outerHTML
+// first.outerHTML = "<div>Hey</div>"
+
+// document.body.firstChild
+// document.body.firstChild.data
+// document.body.firstChild.nodeValue
+console.log(document.body.textContent)
+
+
+
+
+
