040daeee61621e2e27abb41f5d3e02dbfb0c17c5
amq001 <aqibmehtab001@gmail.com>
2024-01-09 15:12:24 +0500

    lecture 42 added

---
+++ 42_Html_attributes_and_methods/script.js
@@ -0,0 +1,17 @@
+let firstval = document.getElementById("first")
+
+let a = first.getAttribute("class") // value of attribute
+console.log(a)
+console.log(first.hasAttribute("class"))    //exist or not
+console.log(first.hasAttribute("style"))
+
+// firstval.setAttribute("hidden","true")
+// firstval.setAttribute("class","true hello")
+
+// firstval.removeAttribute("class")
+
+console.log(first.attributes)
+console.log(first.dataset.game)
+console.log(first.dataset.player)
+
+
