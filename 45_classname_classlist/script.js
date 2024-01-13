0c03546585e80d6ace54e51b60f8b70edc13e9c7
amq001 <aqibmehtab001@gmail.com>
2024-01-13 13:34:13 +0500

    Lecture 45 added

---
+++ 45_classname_classlist/script.js
@@ -0,0 +1,8 @@
+first.className = "dark red"
+first.classList.remove("red")
+first.classList.add("red")
+first.classList.toggle("red")
+first.classList.toggle("red")   //if not then add if yes then remove
+
+console.log(first.classList.contains("red"))
+
