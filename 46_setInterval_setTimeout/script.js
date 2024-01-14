e821088546b60cc036184bdc66de84fbfabe78ef
amq001 <aqibmehtab001@gmail.com>
2024-01-14 14:56:50 +0500

    Lecture 46 added

---
+++ 46_setInterval_setTimeout/script.js
@@ -0,0 +1,21 @@
+document.write("hello")
+
+const sum = (a,b,c) =>{
+    console.log(a+b+c)
+}
+// setTimeout(sum, 5000 , 5 ,6 ,7)
+// setInterval(sum, 1000,5,6,7)
+// setInterval(function(){
+//     alert("hello")
+// },3000)
+
+// let a = setTimeout(function(){
+//     alert("I am inside of set Timeout")
+// },5000)
+
+// let b = prompt("Do you want to run these setTimeOut?")
+// if ("n"==b) {
+//     clearTimeout(a)
+// }
+// console.log(a)
+
