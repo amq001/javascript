1ac8faa397875696af7e97fc92f0abc52043209f
amq001 <aqibmehtab001@gmail.com>
2024-02-01 00:00:08 +0500

    Lecture 62

---
+++ 62_The_Finally_Clause/script.js
@@ -0,0 +1,32 @@
+const f = ()=>{
+    try{
+        // let a = 0
+        // console.log(ariz)
+        console.log("ok hai")
+        return
+    }catch(err){
+        console.log("this is an error")
+        console.log(anas)
+    }
+    finally{
+        console.log("I am a good boy")
+        // Will always run
+    }
+}
+f()
+console.log("end")
+try{
+    let a = 0
+    // console.log(ariz)
+    console.log("ok hai")
+}catch(err){
+    console.log("this is an error")
+    console.log(anas)
+}
+finally{
+    console.log("I am a good boy")
+}
+
+
+
+
