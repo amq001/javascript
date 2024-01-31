f4afd99188d263ecc381f2c9349bf36f2b855dfe
amq001 <aqibmehtab001@gmail.com>
2024-01-31 23:58:28 +0500

    Lecture 60

---
+++ 60_Error_Handling/script.js
@@ -0,0 +1,40 @@
+setTimeout(()=>{
+    console.log("hacking wifi plz wait")
+},1000)
+
+// console.log(aqib)
+
+try{
+    console.log(aqib)
+}
+catch(error){
+    console.log("balle balle " + error)
+}
+
+setTimeout(()=>{
+    console.log("Fetching username and password... please wait")
+},2000)
+setTimeout(()=>{
+    console.log("Hacking Anas's id")
+},3000)
+setTimeout(()=>{
+    console.log("username and password hasbeen fetched")
+},4000)
+
+try{
+    setTimeout(()=>{
+        console.log(ariz)
+    },1000)
+}
+catch(err){
+    console.log("hello")
+}
+
+// setTimeout(()=>{
+//     try{
+//         console.log(ariz)
+//     }
+//     catch(err){
+//         console.log("hello")
+//     }
+// },1000)
