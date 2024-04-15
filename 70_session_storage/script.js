bf4ef73a453c73397f9ff604292893d9e09e81e8
amq001 <aqibmehtab001@gmail.com>
2024-04-15 17:47:57 +0500

    Lecture 70

---
+++ 70_session_storage/script.js
@@ -0,0 +1,10 @@
+// sessionStorage.setItem("name","aqib")
+// sessionStorage.getItem("name")
+// sessionStorage.clear()
+// sessionStorage.removeItem("name")
+// sessionStorage.setItem("name","aqib")
+
+window.onstorage = (e)=>{
+    alert("changed")
+    console.log(e)
+}
